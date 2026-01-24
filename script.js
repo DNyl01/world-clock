1d20eb99bd70cfb9225fa971b78de4d726fde64d
DNyl01 <daranylml@gmail.com>
2026-01-24 18:50:52 +0000

    added moment tz cdn

--- script.js
+++ script.js
@@ -4,33 +4,52 @@ setInterval (updateClock, 1000);
 function updateClock(){
 
 let cabindaEl = document.querySelector("#cabinda");
+if (cabindaEl){
 let cabindaDateEl = cabindaEl.querySelector(".date");
 let cabindaTimeEl = cabindaEl.querySelector(".time");
-cabindaDateEl.innerHTML = moment().format("DD/MM/YYYY");
-cabindaTimeEl.innerHTML = moment().format("hh:mm:ss A");
-
+cabindaDateEl.innerHTML = moment().tz("Africa/Luanda").format("DD/MM/YYYY");
+    cabindaTimeEl.innerHTML = moment().tz("Africa/Luanda").format("hh:mm:ss A");
+}
 
 let coimbraEl = document.querySelector("#coimbra");
+if (coimbraEl){
 let coimbraDatel = coimbraEl.querySelector(".date");
 let coimbraTimeEl = coimbraEl.querySelector(".time");
-coimbraDatel.innerHTML = moment().format("DD/MM/YYYY");
-coimbraTimeEl.innerHTML = moment().format("hh:mm:ss A");
-
+coimbraDatel.innerHTML = moment().tz("Europe/Lisbon").format("DD/MM/YYYY");
+    coimbraTimeEl.innerHTML = moment().tz("Europe/Lisbon").format("hh:mm:ss A");
+}
 
 
 let londresEl = document.querySelector("#londres");
+if (londresEl){
 let londresDateEl = londresEl.querySelector(".date");
 let londresTimeEl = londresEl.querySelector(".time");
-londresDateEl.innerHTML = moment().format("DD/MM/YYYY");
-londresTimeEl.innerHTML = moment().format("hh:mm:ss A");
-
-
-}
-function updateCity(event) {
-  let timeZone = event.target.value;
-
-  if (timeZone !== "") {
-    let cityTime = moment().tz(timeZone).format("hh:mm:ss A");
-    document.querySelector("#city").innerHTML = cityTime;
+londresDateEl.innerHTML = moment().tz("Europe/London").format("DD/MM/YYYY");
+    londresTimeEl.innerHTML = moment().tz("Europe/London").format("hh:mm:ss A");
   }
+
+
 }
+function updateCity(event){
+    let cityTimeZone = event.target.value; 
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesEl = document.querySelector("#cities");
+
+
+let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+
+    citiesEl.innerHTML = 
+ ` <div class="city" 
+    <h2>${cityName} </h2>
+    <div class="time"> ${cityTime.format("MMMM Do YYYY")} </div>
+<div class="date"> ${cityTime.format(" hh:mm:ss A")} </div >
+ </div>
+`
+;
+
+
+
+}
+
+let citySelectEl = document.querySelector("#city");
+citySelectEl.addEventListener("change" , updateCity);
